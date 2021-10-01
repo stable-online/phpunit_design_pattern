53a81cef03b94b948411035b08daf0b1c56a2ff8
zw <2426595849@qq.com>
2021-10-01 23:35:50 +0800

    [task 设计模式 - 模板模式] {丰田车重构}

--- pattern/behavior/template/FTCa.php
+++ pattern/behavior/template/FTCa.php
@@ -4,17 +4,33 @@
  * @User     : zw
  * @DateTime : 2021/10/1 11:22 下午
  */
+
 namespace App\behavior\template;
 
-class FTCa
+/**
+ * @Class   FTCa
+ * @package App\behavior\template
+ */
+class FTCa extends Car
 {
-    /**
-     * @Description:
-     * @DateTime   :    2021/10/1 11:23 下午
-     * @return string
-     */
-    public function getCar(): string
+    public function carName(): string
     {
-        return "丰田车,红色车轱辘,蓝色方向盘,黑色车身";
+        return "丰田车,";
     }
+
+    public function carGl(): string
+    {
+        return "红色车轱辘,";
+    }
+
+    public function fxp(): string
+    {
+        return "蓝色方向盘,";
+    }
+
+    public function cs(): string
+    {
+        return "黑色车身";
+    }
+
 }
