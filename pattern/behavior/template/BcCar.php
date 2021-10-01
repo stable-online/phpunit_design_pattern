281a81c1a8cc34ecc3ee5c0ada4bfd7cdf2e2578
zw <2426595849@qq.com>
2021-10-01 23:41:29 +0800

    [task 设计模式 - 模板模式] {奔驰车实现}

---
+++ pattern/behavior/template/BcCar.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @className:
+ * @User     : zw
+ * @DateTime : 2021/10/1 11:39 下午
+ */
+
+namespace App\behavior\template;
+
+
+class BcCar
+{
+
+    /**
+     * @constructor BcCar.
+     */
+    public function __construct()
+    {
+    }
+}
