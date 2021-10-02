5965f5a63e9c5c339b624e28db877bbe1a0f8dab
zw <2426595849@qq.com>
2021-10-02 14:44:47 +0800

    [task 设计模式 - 观察者模式] 添加被观察者的信息{重构}

--- pattern/behavior/observer/Subject.php
+++ pattern/behavior/observer/Subject.php
@@ -4,25 +4,33 @@
  * @User     : zw
  * @DateTime : 2021/10/2 2:40 下午
  */
-
 namespace App\behavior\observer;
-
 
 use PHPUnit\Framework\MockObject\MockObject;
 
+/**
+ * @Class   Subject
+ * @package App\behavior\observer
+ */
 class Subject
 {
+    /**
+     * @var array
+     */
+    private array $observer;
 
     /**
      * @Description:
      * @DateTime   :    2021/10/2 2:42 下午
      *
-     * @param  Observer|MockObject  $mockObject
+     * @param  Observer|MockObject  $observer
      *
      * @return bool
      */
-    public function attach(Observer|MockObject $mockObject): bool
+    public function attach(Observer|MockObject $observer): bool
     {
+        $this->observer[] = $observer;
+
         return true;
     }
 }
