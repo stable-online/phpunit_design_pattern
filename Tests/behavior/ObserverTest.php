329f98718b98cd93bc35809d5e4b8eaa50482926
zw <2426595849@qq.com>
2021-10-02 15:17:48 +0800

    [task 设计模式 - 观察者模式] 通知观察者{实现.}

--- Tests/behavior/ObserverTest.php
+++ Tests/behavior/ObserverTest.php
@@ -24,8 +24,31 @@ class ObserverTest extends TestCase
     {
         $mockObject = $this->createMock(Observer::class);
 
-        $subject = new Subject();
+        $subject = new Subject("");
 
         $this->assertTrue($subject->attach($mockObject));
+    }
+
+    /**
+     * @Description:    当前观察者接到信息时,被观察者也能接到通知
+     * @DateTime   :    2021/10/2 2:47 下午
+     */
+    public function test_giveNotify()
+    {
+        //定义被观察者1的信息
+        $mockBuilder1 = $this->getMockBuilder(Observer::class)->onlyMethods(['notify'])->getMock();
+        $mockBuilder1->expects($this->once())->method('notify')->with($this->equalTo("hello world"));
+
+        //定义被观察者2的信息
+        $mockBuilder2 = $this->getMockBuilder(Observer::class)->onlyMethods(['notify'])->getMock();
+        $mockBuilder2->expects($this->once())->method('notify')->with($this->equalTo("hello world"));
+
+        //注册观察者
+        $subject = new Subject( "hello world");
+        $subject->attach($mockBuilder1);
+        $subject->attach($mockBuilder2);
+
+        //进行通知
+        $subject->notify();
     }
 }
--- pattern/behavior/observer/Subject.php
+++ pattern/behavior/observer/Subject.php
@@ -15,9 +15,24 @@ use PHPUnit\Framework\MockObject\MockObject;
 class Subject
 {
     /**
+     * @var string
+     */
+    private string $message;
+
+    /**
      * @var array
      */
     private array $observer;
+
+    /**
+     * @constructor Subject.
+     *
+     * @param  string  $message
+     */
+    public function __construct(string $message)
+    {
+        $this->message = $message;
+    }
 
     /**
      * @Description:
@@ -32,5 +47,16 @@ class Subject
         $this->observer[] = $observer;
 
         return true;
+    }
+
+    /**
+     * @Description:
+     * @DateTime   :    2021/10/2 2:58 下午
+     */
+    public function notify()
+    {
+        foreach ($this->observer as $object){
+            $object->notify($this->message);
+        }
     }
 }
