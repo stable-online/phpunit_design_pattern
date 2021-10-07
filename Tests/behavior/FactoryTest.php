fe8b19ffd411976dbbeff6bb13ad41bff7f32bc2
zw <2426595849@qq.com>
2021-10-07 20:54:48 +0800

    [task 行为型设计模式 -- 工厂模式] 添加s1类 属性实现

--- Tests/behavior/FactoryTest.php
+++ Tests/behavior/FactoryTest.php
@@ -17,7 +17,7 @@ use PHPUnit\Framework\TestCase;
 class FactoryTest extends TestCase
 {
     /**
-     * @Description:
+     * @Description:    从工厂中获取s1类型的对象
      * @DateTime   :    2021/10/7 8:39 下午
      */
     public function test_get_S1Type_From_HpFactory()
@@ -25,5 +25,20 @@ class FactoryTest extends TestCase
         $hpFactory = new HpFactory();
 
         $this->assertInstanceOf(S1TypeOfPhone::class,$hpFactory->make("s1"));
+    }
+
+    /**
+     * @Description:    从s1类型的对象中获取属性值
+     * @DateTime   :    2021/10/7 8:50 下午
+     */
+    public function test_get_attr_val_from_S1Type()
+    {
+        $hpFactory = new HpFactory();
+
+        $s1TypeOfPhone = $hpFactory->make("s1");
+
+        $this->assertInstanceOf(S1TypeOfPhone::class, $s1TypeOfPhone);
+
+        $this->assertEquals("s1_attr",$s1TypeOfPhone->getAttr());
     }
 }
--- pattern/behavior/factory/S1TypeOfPhone.php
+++ pattern/behavior/factory/S1TypeOfPhone.php
@@ -4,11 +4,20 @@
  * @User     : zw
  * @DateTime : 2021/10/7 8:44 下午
  */
-
 namespace App\behavior\factory;
 
-
+/**
+ * @Class   S1TypeOfPhone
+ * @package App\behavior\factory
+ */
 class S1TypeOfPhone
 {
-
+    /**
+     * @Description:
+     * @DateTime   :    2021/10/7 8:51 下午
+     */
+    public function getAttr(): string
+    {
+        return "s1_attr";
+    }
 }
