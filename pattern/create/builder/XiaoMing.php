914d04e5ccce474b94411e2d73374a93b0faf741
zw <2426595849@qq.com>
2021-10-08 15:15:48 +0800

    [task 设计模式 创建型] 1. 建造者模式 新增需求{添加小明职业特性.}
    [task 设计模式 创建型] 2.建造者模式 添加小明的属性{实现}
    
    [task 设计模式 创建型] 2. 建造者模式 添加小明属性{重构}

--- pattern/create/builder/XiaoMing.php
+++ pattern/create/builder/XiaoMing.php
@@ -7,43 +7,116 @@
 
 namespace App\create\builder;
 
+use JetBrains\PhpStorm\Pure;
 
 class XiaoMing
 {
     /**
-     * @var string
+     * @var object
      */
-    private string $height;
-    /**
-     * @var string
-     */
-    private string $weight;
-    /**
-     * @var string
-     */
-    private string $hobby;
-    /**
-     * @var string
-     */
-    private string $age;
+    private object $builder;
 
     /**
      * @constructor XiaoMing.
      *
-     * @param  string  $height
-     * @param  string  $weight
-     * @param  string  $hobby
-     * @param  string  $age
+     * @param  object  $builder
      */
-    public function __construct(string $height, string $weight, string $hobby, string $age) {
-        $this->height = $height;
-        $this->weight = $weight;
-        $this->hobby  = $hobby;
-        $this->age    = $age;
+    public function __construct(object $builder) {
+        $this->builder = $builder;
     }
 
+    /**
+     * @Description:
+     * @DateTime   :    2021/10/8 4:37 下午
+     * @return string
+     */
     public function introduce(): string
     {
-        return "身高:{$this->height},体重:{$this->weight},爱好:{$this->hobby},年龄:{$this->age};";
+        return "身高:{$this->builder->height},体重:{$this->builder->weight},爱好:{$this->builder->hobby},年龄:{$this->builder->age};职业:{$this->builder->occupation};";
+    }
+
+    /**
+     * @Description:
+     * @DateTime   :    2021/10/8 4:37 下午
+     */
+    public static function builder(): object
+    {
+        return new class  {
+
+            /**
+             * @var string 身高
+             */
+            public string $height;
+
+            /**
+             * @var string 体重
+             */
+            public string $weight;
+
+            /**
+             * @var string 爱好
+             */
+            public string $hobby;
+
+            /**
+             * @var string 职业
+             */
+            public string $occupation;
+
+            /**
+             * @var string 年龄
+             */
+            public string $age;
+            /**
+             * @Description:    执行
+             * @DateTime   :    2021/10/8 4:38 下午
+             */
+            #[Pure]
+            public function run(): XiaoMing
+            {
+                return new XiaoMing($this);
+            }
+
+            /**
+             * @param  string  $height
+             */
+            public function setHeight(string $height): void
+            {
+                $this->height = $height;
+            }
+
+
+            /**
+             * @param  string  $hobby
+             */
+            public function setHobby(string $hobby): void
+            {
+                $this->hobby = $hobby;
+            }
+
+            /**
+             * @param  string  $occupation
+             */
+            public function setOccupation(string $occupation): void
+            {
+                $this->occupation = $occupation;
+            }
+
+            /**
+             * @param  string  $age
+             */
+            public function setAge(string $age): void
+            {
+                $this->age = $age;
+            }
+
+            /**
+             * @param  string  $weight
+             */
+            public function setWeight(string $weight): void
+            {
+                $this->weight = $weight;
+            }
+        };
     }
 }
